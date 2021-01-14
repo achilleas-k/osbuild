262c91cb70ccda1af3c63fbb50fc64ccbe48bd38
Achilleas Koutsou <achilleas@koutsou.net>
2021-01-14 20:20:42 +0100

    test: use pytest instead of unittest

--- test/mod/test_api.py
+++ test/mod/test_api.py
@@ -6,7 +6,7 @@ import os
 import multiprocessing as mp
 import sys
 import tempfile
-import unittest
+import pytest
 
 import osbuild
 from osbuild.util import jsoncomm
@@ -32,99 +32,92 @@ class APITester(osbuild.api.BaseAPI):
         self.clean = True
 
 
-class TestAPI(unittest.TestCase):
-    """Check API infrastructure"""
-    def setUp(self):
-        self.tmp = tempfile.TemporaryDirectory()
+@pytest.fixture
+def tmpdir():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        yield temp_dir
 
-    def tearDown(self):
-        self.tmp.cleanup()
+def test_basic(tmpdir):
+    # Basic API communication and cleanup checks
+    socket = os.path.join(tmpdir, "socket")
+    api = APITester(socket)
+    with api:
+        with jsoncomm.Socket.new_client(socket) as client:
+            req = {'method': 'echo', 'data': 'Hello'}
+            client.send(req)
+            msg, _, _ = client.recv()
+            assert msg["method"] == "reply"
+            assert req["data"] == msg["data"]
 
-    def test_basic(self):
-        # Basic API communication and cleanup checks
+    assert api.clean
+    assert api.messages == 1
 
-        socket = os.path.join(self.tmp.name, "socket")
-        api = APITester(socket)
-        with api:
-            with jsoncomm.Socket.new_client(socket) as client:
-                req = {'method': 'echo', 'data': 'Hello'}
-                client.send(req)
-                msg, _, _ = client.recv()
-                self.assertEqual(msg["method"], "reply")
-                self.assertEqual(req["data"], msg["data"])
+    # Assert proper cleanup
+    assert api.thread is None
+    assert api.event_loop is None
 
-        self.assertEqual(api.clean, True)
-        self.assertEqual(api.messages, 1)
+def test_reentrancy_guard(tmpdir):
+    socket = os.path.join(tmpdir, "socket")
+    api = APITester(socket)
+    with api:
+        with pytest.raises(AssertionError):
+            with api:
+                pass
 
-        # Assert proper cleanup
-        self.assertIsNone(api.thread)
-        self.assertIsNone(api.event_loop)
+def test_get_arguments(tmpdir):
+    path = os.path.join(tmpdir, "osbuild-api")
+    args = {"options": {"answer": 42}}
+    monitor = osbuild.monitor.BaseMonitor(sys.stderr.fileno())
 
-    def test_reentrancy_guard(self):
-        socket = os.path.join(self.tmp.name, "socket")
-        api = APITester(socket)
-        with api:
-            with self.assertRaises(AssertionError):
-                with api:
-                    pass
+    with osbuild.api.API(args, monitor, socket_address=path) as _:
+        data = osbuild.api.arguments(path=path)
+        assert data == args
 
-    def test_get_arguments(self):
-        tmpdir = self.tmp.name
-        path = os.path.join(tmpdir, "osbuild-api")
-        args = {"options": {"answer": 42}}
-        monitor = osbuild.monitor.BaseMonitor(sys.stderr.fileno())
+def test_exception(tmpdir):
+    # Check that 'api.exception' correctly sets 'API.exception'
+    path = os.path.join(tmpdir, "osbuild-api")
+    args = {}
+    monitor = osbuild.monitor.BaseMonitor(sys.stderr.fileno())
 
-        with osbuild.api.API(args, monitor, socket_address=path) as _:
-            data = osbuild.api.arguments(path=path)
-            self.assertEqual(data, args)
+    def exception(path):
+        with osbuild.api.exception_handler(path):
+            raise ValueError("osbuild test exception")
+        assert False, "api.exception should exit process"
 
-    def test_exception(self):
-        # Check that 'api.exception' correctly sets 'API.exception'
-        tmpdir = self.tmp.name
-        path = os.path.join(tmpdir, "osbuild-api")
-        args = {}
-        monitor = osbuild.monitor.BaseMonitor(sys.stderr.fileno())
+    api = osbuild.api.API(args, monitor, socket_address=path)
+    with api:
+        p = mp.Process(target=exception, args=(path, ))
+        p.start()
+        p.join()
+    assert p.exitcode == 2
+    assert api.error is not None, "Error not set"
+    assert "type" in api.error, "Error has no 'type' set"
+    assert api.error["type"] == "exception", "Not an exception"
+    e = api.error["data"]
+    for field in ("type", "value", "traceback"):
+        assert field in e, f"Exception needs '{field}'"
+    assert e["value"] == "osbuild test exception"
+    assert e["type"] == "ValueError"
+    assert "exception" in e["traceback"]
 
-        def exception(path):
-            with osbuild.api.exception_handler(path):
-                raise ValueError("osbuild test exception")
-            assert False, "api.exception should exit process"
+def test_metadata(tmpdir):
+    # Check that `api.metadata` leads to `API.metadata` being
+    # set correctly
+    path = os.path.join(tmpdir, "osbuild-api")
+    args = {}
+    monitor = osbuild.monitor.BaseMonitor(sys.stderr.fileno())
 
-        api = osbuild.api.API(args, monitor, socket_address=path)
-        with api:
-            p = mp.Process(target=exception, args=(path, ))
-            p.start()
-            p.join()
-        self.assertEqual(p.exitcode, 2)
-        self.assertIsNotNone(api.error, "Error not set")
-        self.assertIn("type", api.error, "Error has no 'type' set")
-        self.assertEqual("exception", api.error["type"], "Not an exception")
-        e = api.error["data"]
-        for field in ("type", "value", "traceback"):
-            self.assertIn(field, e, f"Exception needs '{field}'")
-        self.assertEqual(e["value"], "osbuild test exception")
-        self.assertEqual(e["type"], "ValueError")
-        self.assertIn("exception", e["traceback"])
+    def metadata(path):
+        data = {"meta": "42"}
+        osbuild.api.metadata(data, path=path)
+        return 0
 
-    def test_metadata(self):
-        # Check that `api.metadata` leads to `API.metadata` being
-        # set correctly
-        tmpdir = self.tmp.name
-        path = os.path.join(tmpdir, "osbuild-api")
-        args = {}
-        monitor = osbuild.monitor.BaseMonitor(sys.stderr.fileno())
-
-        def metadata(path):
-            data = {"meta": "42"}
-            osbuild.api.metadata(data, path=path)
-            return 0
-
-        api = osbuild.api.API(args, monitor, socket_address=path)
-        with api:
-            p = mp.Process(target=metadata, args=(path, ))
-            p.start()
-            p.join()
-            self.assertEqual(p.exitcode, 0)
-        metadata = api.metadata  # pylint: disable=no-member
-        assert metadata
-        self.assertEqual(metadata, {"meta": "42"})
+    api = osbuild.api.API(args, monitor, socket_address=path)
+    with api:
+        p = mp.Process(target=metadata, args=(path, ))
+        p.start()
+        p.join()
+        assert p.exitcode == 0
+    metadata = api.metadata  # pylint: disable=no-member
+    assert metadata
+    assert metadata == {"meta": "42"}
